4d51f3516a11c8cc281de0550d6fb623fc1efa1b
Zhighway777 <1990405801@qq.com>
2024-08-29 14:59:02 +0800

    update files

--- linpack/liyangtst/0815tst.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-extern void vlenbprint(int *p);
-int main()
-{
-    int p;
-    vlenbprint(&p);
-    printf("%d",p);
-    return 0;
-}
--- linpack/liyangtst/vlenbprint.s
+++
@@ -1,5 +0,0 @@
-.section .text
-.globl vlenbprint
-vlenbprint:
-    csrr    t0, vlenb
-    sw      a0, 0(t0)
